4b8a7b4ff27cfc409cb729187878beeaab06faa6
James Allsopp <j.allsopp@bham.ac.uk>
2023-06-29 14:29:50 +0100

    additional test

--- dice_package/test_die.py
+++ dice_package/test_die.py
@@ -3,3 +3,9 @@ from .dice import RollableDie
 def test_geometry():
         die = RollableDie()
         assert die.sides==6
+
+def test_roll():
+    die = RollableDie()
+    options=list(range(1,die.sides+1))
+    roll = die.roll()
+    assert roll in options
